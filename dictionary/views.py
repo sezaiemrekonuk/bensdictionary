02af506fc8dba278b66dd303aa37bc477e6004da
Sezai Emre Konuk <sezaikonuk@hacettepe.edu.tr>
2023-04-13 22:37:42 +0300

    search page specs

--- dictionary/views.py
+++ dictionary/views.py
@@ -11,13 +11,35 @@ def search(request):
     if request.method == 'POST':
         searchInput = request.POST.get('search-input')
         searchType = request.POST.get('search-type')
-        
-        if searchType == 'Tüm Türler':
-            context = { 'searchQuery': searchInput, 'searchResults': searcher.searcher(searchInput) }
+        if (len(searcher.searcher(searchInput)) != 0):
+            if searchType == 'Tüm Türler':
+                result = searcher.searcher(searchInput)[0]
+                downSide = [Structure.get.filter(id=obj).slug for obj in range(result.id, result.id)]
+                
+                if (result.id <= 5 or not Structure.get.filter(id=result.id) == None):
+                    topSide = [Structure.get.filter(id=obj).slug for obj in range(result.id, result.id)]
+                else:
+                    topSide = [Structure.get.filter(id=obj).slug for obj in range(result.id, result.id)]
+                    
+                
+                context = { 'searchQuery': searchInput, 'searchResults': result, 'topSide': topSide, 'downSide': downSide }
+            else:
+                result = searcher.searcher(searchInput, searchType)[0]
+                downSide = [Structure.get.filter(id=obj).slug for obj in range(result.id, result.id)]
+                
+                if (result.id <= 5 or not Structure.get.filter(id=result.id) == None):
+                    topSide = [Structure.get.filter(id=obj).slug for obj in range(result.id, result.id)]
+                else:
+                    topSide = [Structure.get.filter(id=obj).slug for obj in range(result.id, result.id)]
+                    
+                
+                context = { 'searchQuery': searchInput, 'searchResults': result, 'topSide': topSide, 'downSide': downSide }
+                
+            return render(request, 'dictionary/search.html', context)
+
         else:
-            context = { 'searchQuery': searchInput, 'searchResults': searcher.searcher(searchInput, searchType) }
-            
-        return render(request, 'dictionary/search.html', context)
-    
+            return render(request, 'dictionary/about.html')
+
+        
     else:
         return render(request, 'dictionary/index.html')
